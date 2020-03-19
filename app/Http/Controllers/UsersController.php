df5b505a1a5fec6c46d1a51b64967d295db01ac6
fusen daniel <fusendaniel20020115@gmail.com>
2020-03-19 07:34:37 +0000

    twitter

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -31,8 +31,6 @@ class UsersController extends Controller
             
             $data += $this->counts($user);
             
-            
-            
             return view('users.show',$data);
     }
     
